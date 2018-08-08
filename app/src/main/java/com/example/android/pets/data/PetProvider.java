e13a747d9826a9aec162f3c2db358f162da175da
Hossam Tarek <hossamtarek250@gmail.com>
2018-08-08 19:30:31 +0200

    Implement delete all pets option menu in catalog activity

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -219,6 +219,7 @@ public class PetProvider extends ContentProvider {
                 selection = PetEntry._ID + "=?";
                 selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
                 rowsDeleted = database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+                break;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " + uri);
         }
